bacacc11f1f0dc72e5e2e97bcaec132cf6d28eba
bintangginanjar <bintang.ginanjar@gmail.com>
2025-02-10 13:40:42 +0700

    Change token expiration

--- src/main/java/com/runclub/restful/api/security/SecurityConstants.java
+++ src/main/java/com/runclub/restful/api/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.runclub.restful.api.security;
 
 public class SecurityConstants {
 
-    public static final int JWTexpiration = 720000000;
+    public static final int JWTexpiration = 72000000;
 
     public static final String JWTsecret = "secret";
 
